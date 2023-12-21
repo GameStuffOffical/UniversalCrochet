b50d6b2913ac8a20dcb879cb3502cb4501ffee30
GameStuffOffical <66188005+GameStuffOffical@users.noreply.github.com>
2023-12-21 16:56:05 -0600

    h

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+money = 10
 stitches = 0
 yarn = 1000
+yarnCost = 5
 
 function crochetStitch() {
     if (yarn > 0) {
@@ -8,9 +10,18 @@ function crochetStitch() {
     }
 }
 
+function buyYarn(){
+    if (money >= yarnCost) {
+        money -= yarnCost
+        yarn += 1000
+    }
+}
+
 function updateGUI(){
     document.getElementById("stitches").innerHTML = stitches
+    document.getElementById("money").innerHTML = money
     document.getElementById("yarn").innerHTML = yarn
+    document.getElementById("yarn-cost").innerHTML = yarnCost
 }
 
 function mainLoop(){
